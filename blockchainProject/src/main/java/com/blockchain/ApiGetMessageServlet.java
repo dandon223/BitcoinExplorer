78b0c7c626ebcbee074d812ae4b382283bfddf70
dandon223 <danielggg@onet.pl>
2021-05-06 19:50:21 +0200

    shows BTC/USD price

--- blockchainProject/src/main/java/com/blockchain/ApiGetMessageServlet.java
+++ blockchainProject/src/main/java/com/blockchain/ApiGetMessageServlet.java
@@ -7,6 +7,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ApiGetMessageServlet extends HttpServlet {
   
@@ -16,9 +23,29 @@ public class ApiGetMessageServlet extends HttpServlet {
       throws ServletException, IOException {
       
       response.setContentType("application/json");
-
-      PrintWriter out = response.getWriter();
-      out.println("{ \"message\": \"The board is green!\" }");  
+      HttpClient httpClient = HttpClient.newBuilder().build();
+      HttpRequest requestx;
+      try {
+         PrintWriter out = response.getWriter();
+         requestx = HttpRequest.newBuilder(new URI("https://rest.coinapi.io/v1/exchangerate/BTC/USD"))
+            .headers("X-CoinAPI-Key", "3707CA9C-9195-4C2C-925D-64E49EA413B6").GET().build();
+            HttpResponse<String> responsex = httpClient.send(requestx, HttpResponse.BodyHandlers.ofString());
+            String str = "[" + responsex.body() + "]";
+            // print response body
+            //System.out.println(responsex.body());
+            JSONArray array = new JSONArray(str);
+            JSONObject object = array.getJSONObject(0);
+            double money = object.getDouble("rate");
+      
+            
+            out.println("{ \"BTC/USD\": \""+money+" \" }"); 
+      } catch (URISyntaxException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
       
    }
 
--- blockchainProject/src/test/java/AppTest.java
+++ blockchainProject/src/test/java/AppTest.java
@@ -1,4 +1,4 @@
-package com.blockchain;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
