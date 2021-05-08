bb5454de8197fa41ff0857e79a207f637b35c000
ProtegerPW <kobolte@gmail.com>
2021-05-08 15:25:01 +0200

    Init frontend

---
+++ blockchainProject/src/main/ui/front_explorer/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {useEffect, useState} from "react";
+
+function App() {
+  const[readCoin, setReadCoin] = useState(null);
+
+  useEffect(() => {
+    console.log("Done before loading page");
+
+    if(!readCoin){
+      fetch("http://localhost:8080/blockchainProject-1.0-SNAPSHOT/api/getMessage")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Current bCoin rate: ", data);
+        setReadCoin(data);
+      });
+    }
+  }, [readCoin]);
+  return (
+    <div>
+      {readCoin ? ((readCoinItem) => {
+        return <div>Hellaa</div>
+        
+      })
+      : "loading data ... "}
+      </div>
+  );
+}
+
+export default App;
