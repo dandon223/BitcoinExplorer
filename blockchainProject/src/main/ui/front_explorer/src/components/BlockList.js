da1a7ad575fa0febac79e215d5d0765cf28711a5
ProtegerPW <kobolte@gmail.com>
2021-05-21 17:22:37 +0200

    Code refactoring

--- blockchainProject/src/main/ui/front_explorer/src/components/BlockList.js
+++ blockchainProject/src/main/ui/front_explorer/src/components/BlockList.js
@@ -1,54 +1,9 @@
 import React, {useEffect, useState} from 'react';
 
-// class BlockList extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             readBlock: null,
-//         };
-//     }
-
-
-//     render() {
-//         this.readBlock();
-//         return (
-//             <div>
-//             {this.state.readBlock ? this.state.readBlock.map((blockItem, i) => {
-//                 return (
-//                     <div>
-//                         <tr key={i}>
-//                             <td>{blockItem.nonce}</td>
-//                         </tr>
-//                     </div>
-//                 );
-//             }) 
-//         : null}
-//             </div>);
-//     }
-    
-
-
-//     readBlock = () => {
-//         if(!this.state.readBlock){
-//                 fetch("http://localhost:8080/blockchainProject-2.0-SNAPSHOT/api/getBlock?hash=" + this.props.blockByHash)
-//                 .then((response) => response.json())
-//                 .then((data) => {
-//                     this.setState({readBlock: data});
-//                 });
-//             } 
-//         }
-
-// function BlockList(props) {
-//     return 
-//     <li>{props.blockByHash}</li>
-// }
-// }
-
 function BlockList(props) {
     const[readBlock, setreadBlock] = useState("");
 
     const [blockTrans, setBlockTrans] = useState([])
-
 
     useEffect(() => {
         if(props.blockByHash != ""){
@@ -124,9 +79,3 @@ function BlockList(props) {
     }
   }
 export default BlockList;
-
-function toDateTime(secs) {
-    var t = new Date(1970, 0, 1); // Epoch
-    t.setSeconds(secs);
-    return t;
-}
--- blockchainProject/src/main/ui/front_explorer/src/components/GetBlock.js
+++ blockchainProject/src/main/ui/front_explorer/src/components/GetBlock.js
@@ -17,12 +17,6 @@ class GetBlock extends React.Component {
   };
 
   render() {
-    // const isHash = this.state.readHash;
-    // let blockByHash;
-    // if(isHash) {
-    //   blockByHash =
-    // }
-
     return (
       <div>
         <GetBlockHash handleHashProps={this.handleHash}/>
@@ -33,39 +27,3 @@ class GetBlock extends React.Component {
 }
 
 export default GetBlock;
-
-// function GetBlock() {
-//     const[readBlock, setreadBlock] = useState(null);
-
-//     useEffect(() => {
-
-//     if(!readBlock){
-//         fetch("http://localhost:8080/blockchainProject-1.0-SNAPSHOT/api/getBlock")
-//         .then((response) => response.json())
-//         .then((data) => {
-//           setreadBlock(data);
-//         });
-//       } 
-//     }, [readBlock]);
-//     return (
-//       <div>
-//         {readBlock ? readBlock.map((blockItem, i) => {
-//             return (
-//                 <div>
-//                     <tr key={i}>
-//                         <td>{blockItem.nonce}</td>
-//                     </tr>
-//                 </div>
-//             );
-//         }) 
-//     : "loading data ..."}
-//       </div>);
-//   }
-  
-//   export default GetBlock;
-// 0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103
-// db654f683af4c5e7d00567c9e656bdc53e25c445b57f115808f2e170242d9cdd
-// 6f5d841d9d4c959384da4d1e9c23cc1e27b9bd319abe0363dde8cb78fcc53f3a
-// a47f54f40c487e307dd8666d5aead263938017dff683b5ab3bf94dc2a5e55d05
-
-// 104061
