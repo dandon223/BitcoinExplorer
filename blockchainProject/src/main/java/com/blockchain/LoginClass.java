d2c65adec7e02c938a9828387d42980caec502a6
Michal <MSmiki1999@gmail.com>
2021-05-21 14:46:03 +0200

    initial tests

--- blockchainProject/src/main/java/com/blockchain/LoginClass.java
+++ blockchainProject/src/main/java/com/blockchain/LoginClass.java
@@ -8,20 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ApiServlet extends HttpServlet {
-  
-  private static final long serialVersionUID = 1L;
+public class LoginClass extends HttpServlet {
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
       
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-      String str= "./api works. Try <a href='./api/getMessage'>./api/getMessage</a>";
+      String str= "class for testing user logins";
       out.println(str);
-      str= "login tests Try <a href='./api/getMessage'>./api/getMessage</a>";
-      out.println(str);
+      
    }
-
 }
